dcdc308cd817b2f432d736c276e6f4899b90d761
spaceship9999 <chowraynold@gmail.com>
2022-03-25 23:42:55 +0000

    Rewrite setup function of category listing page

--- pages/category/[slug]/page/[page_num]/index.vue
+++ pages/category/[slug]/page/[page_num]/index.vue
@@ -3,10 +3,28 @@
 </template>
 
 <script>
+import {useUserStore} from "../../../../../stores/useUserStore";
+
+
 export default {
   name: "CategoryPage",
   async setup() {
+    const user = useUserStore();
+    const route = useRoute();
+    const { ssrContext } = useNuxtApp()
 
+    if(user.isLoggedIn) {
+      user.setAuthHeader()
+    }
+
+
+    const category_req = await useNuxtApp().$axios.get('/api/category/' + route.params.slug)
+    const category_data = await category_req.data
+
+    return {
+      user,
+      category_data,
+    }
   }
 }
 </script>
